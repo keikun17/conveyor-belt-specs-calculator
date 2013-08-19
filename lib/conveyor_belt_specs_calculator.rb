175af5751056436e09c143754ed262e2297f7433
Buddy Magsipoc <keikun17@gmail.com>
2013-08-19 20:30:45 +0800

    Require libs.

--- lib/conveyor_belt_specs_calculator.rb
+++ lib/conveyor_belt_specs_calculator.rb
@@ -1,4 +1,7 @@
 require "conveyor_belt_specs_calculator/version"
+require "conveyor_belt_specs_calculator/belt"
+require "conveyor_belt_specs_calculator/usage"
+require "conveyor_belt_specs_calculator/calculator"
 
 module ConeyorBeltSpecsCalculator
   # Your code goes here...
